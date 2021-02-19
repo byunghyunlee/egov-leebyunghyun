4f64939a9cebd1d674485e83d4a0f6bdf1a46a36
Dimsum <Dimsum@DESKTOP-O2MUMG1>
2021-02-19 12:35:17 +0900

    중간커밋

--- src/main/java/edu/human/com/admin/web/AdminController.java
+++ src/main/java/edu/human/com/admin/web/AdminController.java
@@ -1,11 +1,15 @@
 package edu.human.com.admin.web;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,14 +17,87 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.human.com.member.service.EmployerInfoVO;
 import edu.human.com.member.service.MemberService;
+import edu.human.com.util.PageVO;
+import egovframework.com.cmm.LoginVO;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
+import egovframework.let.cop.bbs.service.BoardMasterVO;
+import egovframework.let.cop.bbs.service.BoardVO;
+import egovframework.let.cop.bbs.service.EgovBBSAttributeManageService;
+import egovframework.let.cop.bbs.service.EgovBBSManageService;
 import egovframework.let.utl.sim.service.EgovFileScrty;
+import egovframework.rte.fdl.property.EgovPropertyService;
+import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
 
 @Controller
 public class AdminController {
 	
 	@Inject
 	private MemberService memberService; 
+	//스프링빈(new키워드만드는 오브젝트x) 오브젝트를 사용하는 방법 @Inject, @Autowired, @Resource
+	@Autowired
+	private EgovBBSAttributeManageService bbsAttrbService;
+	@Autowired
+	private EgovPropertyService propertyService;
+	@Autowired
+	private EgovBBSManageService bbsMngService;
+
 	
+	@RequestMapping("/admin/board/list_board.do")
+	public String list_board(@ModelAttribute("searchVO") BoardVO boardVO, ModelMap model) throws Exception {
+		LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+
+		boardVO.setBbsId(boardVO.getBbsId());
+		boardVO.setBbsNm(boardVO.getBbsNm());
+
+		BoardMasterVO vo = new BoardMasterVO();
+		System.out.println("디버그: 게시판아이디 "+boardVO.getBbsId());
+		vo.setBbsId(boardVO.getBbsId());
+		vo.setUniqId(user.getUniqId());
+
+		BoardMasterVO master = bbsAttrbService.selectBBSMasterInf(vo);
+
+		//-------------------------------
+		// 방명록이면 방명록 URL로 forward
+		//-------------------------------
+		if (master.getBbsTyCode().equals("BBST04")) {
+		    return "forward:/cop/bbs/selectGuestList.do";
+		}
+		////-----------------------------
+
+		boardVO.setPageUnit(propertyService.getInt("pageUnit"));
+		boardVO.setPageSize(propertyService.getInt("pageSize"));
+
+		PaginationInfo paginationInfo = new PaginationInfo();
+
+		paginationInfo.setCurrentPageNo(boardVO.getPageIndex());
+		paginationInfo.setRecordCountPerPage(boardVO.getPageUnit());
+		paginationInfo.setPageSize(boardVO.getPageSize());
+
+		boardVO.setFirstIndex(paginationInfo.getFirstRecordIndex());
+		boardVO.setLastIndex(paginationInfo.getLastRecordIndex());
+		boardVO.setRecordCountPerPage(paginationInfo.getRecordCountPerPage());
+
+		Map<String, Object> map = bbsMngService.selectBoardArticles(boardVO, vo.getBbsAttrbCode());
+		int totCnt = Integer.parseInt((String)map.get("resultCnt"));
+
+		paginationInfo.setTotalRecordCount(totCnt);
+
+		//-------------------------------
+		// 기본 BBS template 지정
+		//-------------------------------
+		if (master.getTmplatCours() == null || master.getTmplatCours().equals("")) {
+		    master.setTmplatCours("/css/egovframework/cop/bbs/egovBaseTemplate.css");
+		}
+		////-----------------------------
+
+		model.addAttribute("resultList", map.get("resultList"));
+		model.addAttribute("resultCnt", map.get("resultCnt"));
+		model.addAttribute("boardVO", boardVO);
+		model.addAttribute("brdMstrVO", master);
+		model.addAttribute("paginationInfo", paginationInfo);
+		
+		return "admin/board/list_board";
+	}
 	@RequestMapping(value="/admin/member/delete_member.do",method=RequestMethod.POST)
 	public String delete_member(EmployerInfoVO memberVO,RedirectAttributes rdat) throws Exception {
 		memberService.deleteMember(memberVO.getEMPLYR_ID());
@@ -28,7 +105,7 @@ public class AdminController {
 		return "redirect:/admin/member/list_member.do";
 	}
 	@RequestMapping(value="/admin/member/view_member.do",method=RequestMethod.GET)
-	public String view_member(Model model,@RequestParam("emplyr_id") String emplyr_id) throws Exception {
+	public String view_member(@ModelAttribute("pageVO") PageVO pageVO,Model model,@RequestParam("emplyr_id") String emplyr_id) throws Exception {
 		//회원 보기[수정] 페이지 이동.
 		EmployerInfoVO memberVO = memberService.viewMember(emplyr_id);
 		model.addAttribute("memberVO", memberVO);
@@ -73,10 +150,19 @@ public class AdminController {
 	}
 	
 	@RequestMapping(value="/admin/member/list_member.do",method=RequestMethod.GET)
-	public String list_member(Model model) throws Exception {
+	public String list_member(Model model,@ModelAttribute("pageVO") PageVO pageVO) throws Exception {
 		//회원관리 페이지 이동.
-		List<EmployerInfoVO> listMember = memberService.selectMember();
+		if(pageVO.getPage() ==null) {
+			pageVO.setPage(1);
+		}
+		pageVO.setPerPageNum(5);//하단의 페이징 보여줄 개수
+		pageVO.setQueryPerPageNum(10);//쿼리에서 1페이지당 보여줄 개수=화면에서 1페이지당 보여줌		
+		List<EmployerInfoVO> listMember = memberService.selectMember(pageVO);
+		//전체페이지 개수는 자동계산 = total 카운트를 계산 순간(아래)
+		System.out.println("현재 검색된 회원의 total 카운트는 : " + listMember.size());
+		pageVO.setTotalCount(listMember.size());
 		model.addAttribute("listMember", listMember);
+		//model.addAttribute("pageVO", pageVO); jsp로 보내는 대신에 @ModelAttribute사용
 		return "admin/member/list_member";
 	}
 	@RequestMapping(value="/admin/home.do", method=RequestMethod.GET)
